008af830a8496b8edce4ac9af07cad32913dd01d
AF <100952220+ibnbayo@users.noreply.github.com>
2023-08-11 12:59:36 +0100

    Add launch images

--- src/components/Launch/index.js
+++ src/components/Launch/index.js
@@ -2,18 +2,23 @@ import styles from './launch.module.css'
 import moment from 'moment'
 
 const Launch = ({props}) => {
+    console.log(props)
+
+    const imageIndex = Math.round(Math.random() * 5)
 
     return (
        
 
-
+<main> 
         <div className={styles.card}>
             <div className={styles.launchTitle}>{props.name}</div>
   
-  <p>Flight Number: {props.flight_number}</p>
+  {/* <p>Flight Number: {props.flight_number}</p> */}
+  <img src={props.rocket.flickr_images[imageIndex]} alt={props.name}></img>
   <p>Launch Date: {moment(props.date_utc).format('MMMM Do YYYY, h:mm:ss a')}</p>
   
 </div>
+</main>
 
     )
 
